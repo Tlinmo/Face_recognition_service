e0a0c609d70c85b7f10de4333140ae3aa5e94eb0
Dallinchi <herooopr@gmail.com>
2024-11-19 20:45:35 +0300

    Code review

--- app/services/auth/embedding.py
+++ app/services/auth/embedding.py
@@ -12,20 +12,3 @@ class Embedding:
         self.id = id
         self.vector = vector
         self.user_id = user_id
-        
-        
-        
-class User:
-    def __init__(
-        self,
-        username: str | None = None,
-        hashed_password: str | None = None,
-        id: uuid.UUID | None = None,
-        is_superuser: bool = False,
-        embeddings: List[List[float]] = [],
-    ) -> None:
-        self.id = id
-        self.username = username
-        self.hashed_password = hashed_password
-        self.is_superuser = is_superuser
-        self.embeddings = embeddings
